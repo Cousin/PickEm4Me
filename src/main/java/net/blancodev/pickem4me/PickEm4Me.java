0626c484b96af3550409e6fbc5ba229302f4432d
Joey <b3tanyan@gmail.com>
2021-09-14 19:17:32 -0400

    update power rankings and redraw

--- src/main/java/net/blancodev/pickem4me/PickEm4Me.java
+++ src/main/java/net/blancodev/pickem4me/PickEm4Me.java
@@ -1,16 +1,18 @@
 package net.blancodev.pickem4me;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class PickEm4Me {
 
+    private static final Set<NFLTeam> ALREADY_CHOSEN = new HashSet<>();
+    private static final int CURRENT_WEEK = 2;
+
     public static void main(String[] args) {
+
+        ALREADY_CHOSEN.add(NFLTeam.SAN_FRANCISCO_49ERS);
 
         printPicksStats(getPicksMethod1());
         printPicksStats(getPicksMethod2());
-
 
     }
 
@@ -18,13 +20,13 @@ public class PickEm4Me {
 
         final List<NFLGame> picks = new ArrayList<>();
 
-        for (NFLWeek nflWeek : getWeeksStartingAt(1)) {
+        for (NFLWeek nflWeek : getWeeksStartingAt(CURRENT_WEEK)) {
 
             NFLGame chosenGame = null;
 
             for (NFLGame nflGame : nflWeek.getGames()) {
                 if (chosenGame == null || nflGame.getPowerDifference() > chosenGame.getPowerDifference()) {
-                    if (picks.stream().noneMatch(g -> g.getHigherPoweredTeam() == nflGame.getHigherPoweredTeam())) {
+                    if (!ALREADY_CHOSEN.contains(nflGame.getHigherPoweredTeam()) && picks.stream().noneMatch(g -> g.getHigherPoweredTeam() == nflGame.getHigherPoweredTeam())) {
                         chosenGame = nflGame;
                     }
                 }
@@ -42,13 +44,13 @@ public class PickEm4Me {
 
         final List<NFLGame> picks = new ArrayList<>();
 
-        for (NFLWeek nflWeek : getWeeksStartingAt(1)) {
+        for (NFLWeek nflWeek : getWeeksStartingAt(CURRENT_WEEK)) {
 
             NFLGame chosenGame = null;
 
             for (NFLGame nflGame : nflWeek.getGames()) {
                 if (chosenGame == null || nflGame.getPowerDifference() > chosenGame.getPowerDifference()) {
-                    if (picks.stream().noneMatch(g -> g.getHigherPoweredTeam() == nflGame.getHigherPoweredTeam())) {
+                    if (!ALREADY_CHOSEN.contains(nflGame.getHigherPoweredTeam()) && picks.stream().noneMatch(g -> g.getHigherPoweredTeam() == nflGame.getHigherPoweredTeam())) {
                         //chosenGame = nflGame;
 
                         boolean foundBetterWeek = false;
@@ -103,7 +105,7 @@ public class PickEm4Me {
 
         for (NFLGame pick : picks) {
 
-            System.out.println(String.valueOf(picks.indexOf(pick) + 1) + ": " + pick.getHigherPoweredTeam() + " - " + pick.getPowerDifference() + " vs " + pick.getLowerPoweredTeam());
+            System.out.println(String.valueOf(picks.indexOf(pick) + CURRENT_WEEK) + ": " + pick.getHigherPoweredTeam() + " - " + pick.getPowerDifference() + " vs " + pick.getLowerPoweredTeam());
 
         }
 
